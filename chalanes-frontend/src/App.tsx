ee99d894ae2d46be82a0192e650697aa024ea491
Lu Castro <81131054+Lu-2801@users.noreply.github.com>
2026-04-04 13:59:24 -0300

    Agrego analitycs

--- chalanes-frontend/src/App.tsx
+++ chalanes-frontend/src/App.tsx
@@ -4,17 +4,21 @@ import { Home } from './pages/Home';
 import { Services } from './pages/Services';
 import { Booking } from './pages/Booking';
 import { Admin } from './pages/Admin';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="services" element={<Services />} />
-        <Route path="booking" element={<Booking />} />
-        <Route path="admin" element={<Admin />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="booking" element={<Booking />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
